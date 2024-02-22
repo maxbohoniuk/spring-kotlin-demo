279cba2a659a6f8934d080d3925734b651ce7e1e
maxbohoniuk <maxbohoniuk@gmail.com>
2024-02-22 19:25:03 +0100

    Get user by id

--- src/main/kotlin/com/maxbohoniuk/springkotlindemo/users/UserController.kt
+++ src/main/kotlin/com/maxbohoniuk/springkotlindemo/users/UserController.kt
@@ -4,10 +4,12 @@ import com.maxbohoniuk.springkotlindemo.users.model.UserRequestDto
 import com.maxbohoniuk.springkotlindemo.users.model.UserResponseDto
 import com.maxbohoniuk.springkotlindemo.users.service.UserService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @RequestMapping("/users")
@@ -16,6 +18,10 @@ class UserController(private val userService: UserService) {
     @GetMapping
     fun getUsers(): List<UserResponseDto> = userService.getUsers().map { UserResponseDto(it) }
 
+    @GetMapping("/{uuid}")
+    fun getUser(@PathVariable(name = "uuid") uuid: UUID): UserResponseDto {
+        return UserResponseDto(userService.getUserByUUID(uuid))
+    }
     @PostMapping
     fun createUser(@RequestBody user: UserRequestDto): UserResponseDto = UserResponseDto(userService.createUser(user.toEntity()))
 }
--- src/main/kotlin/com/maxbohoniuk/springkotlindemo/users/service/UserService.kt
+++ src/main/kotlin/com/maxbohoniuk/springkotlindemo/users/service/UserService.kt
@@ -2,15 +2,17 @@ package com.maxbohoniuk.springkotlindemo.users.service
 
 import com.maxbohoniuk.springkotlindemo.users.model.User
 import com.maxbohoniuk.springkotlindemo.users.repo.UserReposiroty
+import org.springframework.http.HttpStatusCode
 import org.springframework.stereotype.Service
-import java.time.LocalDateTime
+import org.springframework.web.server.ResponseStatusException
 import java.util.UUID
 
 @Service
 class UserService(private val userReposiroty: UserReposiroty) {
 
     fun getUserByUUID(uuid: UUID): User {
-        return userReposiroty.findById(uuid).orElseThrow()
+        return userReposiroty.findById(uuid)
+            .orElseThrow { ResponseStatusException(HttpStatusCode.valueOf(404), "User not exist") }
     }
 
     fun createUser(user: User): User {
